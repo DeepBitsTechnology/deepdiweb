4b62229c76c083d89dc68f6af8932f7b88088f59
Sentinent <thelin.json@gmail.com>
2022-03-02 14:23:53 -0800

    Immediately exit if DEEPDI_URL is not set

--- proxy/src/config.ts
+++ proxy/src/config.ts
@@ -1,6 +1,11 @@
-import process from 'process';
+import process, { exit } from 'process';
 
 export const UPLOAD_DIR = process.env.UPLOAD_DIR || '/tmp/cache';
 export const MAX_PROJECTS_CACHED = Number.parseInt(process.env.MAX_PROJECTS_CACHED || '100');
 export const MAX_UPLOADS = Number.parseInt(process.env.MAX_UPLOADS || '50');
-export const DEEPDI_URL = process.env.DEEPDI_URL;
+
+export const DEEPDI_URL = process.env.DEEPDI_URL || '';
+if (DEEPDI_URL === '') {
+    console.error('DEEPDI_URL is not set, exiting.');
+    exit(1);
+}
